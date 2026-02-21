c7e127e7aad3ae5743e6818d7844cd50c167bac4
vsey <usokk@student.kit.edu>
2026-02-21 21:33:08 +0100

    use ip instead of host name

--- test.py
+++ test.py
@@ -5,14 +5,12 @@ def test_latency(net, file_name: str, result_dir: Path = Path(".")):
     result_dir = result_dir.expanduser().resolve()
     result_dir.mkdir(parents=True, exist_ok=True)
 
-    isp_user = net["isp_user"]
-    isp_vpn = net["isp_vpn"]
-    internet_core = net["internet_core"]
 
-    server = net["vpn"]
-    client = net["user"]
+    vpn = net["vpn"]
+    service = net["service"]
+    user = net["user"]
 
-    out = client.cmd("ping -c 30 service")
+    out = user.cmd(f"ping -c 30 {service.IP()}")
 
     with open(result_dir / file_name, "w") as f:
         f.write(out)
